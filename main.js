6d2fa269f5696a43114d2bc9506fa127260edd72
Maksim Strikanov <maxvell.88@mail.ru>
2024-03-08 09:51:24 +0300

    Плавный скролл

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ import './style.scss'
 
 import insertServisesCards from './public/components/servises';
 import accordion from './public/components/accordion';
+import handleMarquee from './public/components/infinitiRunText';
+import links from './public/components/links';
 
 accordion()
 insertServisesCards()
+handleMarquee()
+links()
 
---
+++ public/components/links.js
@@ -0,0 +1,16 @@
+const links = async () => { 
+    const links = document.querySelectorAll('.header__item_link')
+  
+
+    links.forEach((item, i) => {
+        item.onclick = () => {
+            document.getElementById(item.getAttribute('data-link')).scrollIntoView(
+            {
+                behavior: "smooth"
+            }
+            )
+        } 
+    });
+} 
+
+export default links;
