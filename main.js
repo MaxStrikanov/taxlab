ddcb2811de57d327c661ea5436261328848348f5
MaxStrikanov <maxvell.88@mail.ru>
2024-03-12 16:13:53 +0300

    Мобильная версии

---
+++ main.js
@@ -0,0 +1,13 @@
+import './style.scss'
+
+import insertServisesCards from './public/components/servises';
+import accordion from './public/components/accordion';
+import handleMarquee from './public/components/infinitiRunText';
+import links from './public/components/links';
+import SmoothScroll from './public/components/SmoothScroll';
+
+SmoothScroll();
+accordion()
+insertServisesCards()
+handleMarquee()
+links()
---
+++ public/components/infinitiRunText.js
@@ -0,0 +1,7 @@
+const handleMarquee = () => {
+    
+};
+
+
+
+export default handleMarquee;
